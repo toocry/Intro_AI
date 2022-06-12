f47cd7b9583416942d251bc4710a31d997be4035
mrDumbledor <59411522+mrDumbledor@users.noreply.github.com>
2022-06-12 17:55:15 +0200

    Update total_sat.py

--- CSP&SAT/total_sat.py
+++ CSP&SAT/total_sat.py
@@ -3,16 +3,6 @@ from pysat.solvers import Glucose3
 
 def total_coloring(graph):
 
-    """
-           Find total chromatic index and total coloring.
-           graph - instance of networkx.Graph
-           returns - total chromatic index x
-           Furthermore, assign property "color" for every vertex and edge. The value of the color has to be an integer between 0 and x-1.
-
-           TODO: The implementation of this function finds some total coloring but the number of colors may be minimal.
-           Find the total chromatic index.
-       """
-
     max_deg = 0
     node_nums = {}
     node_num = 0
